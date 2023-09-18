f4cbe246e656ed43fed2167a42f0c86320bb3070
EduardoCGarcia <echavezg004@gmail.com>
2023-09-18 08:57:03 -0600

    separacion de la base de datos

--- backend/app.js
+++ backend/app.js
@@ -1,1 +1,21 @@
-console.log("Hola");
+require('dotenv').config();
+const express = require('express');
+
+const dbConection = require('./config/mysql');
+
+const PORT = process.env.PORT;
+const app = express();
+
+
+app.get('/', function (req, res) {
+    res.send('Hello World')
+})
+
+const startServer = async () => {
+    await dbConection()
+    app.listen(PORT, ()=> {
+        console.log(`Servidor corriendo en http://localhost:${PORT}`);
+    })
+}
+
+startServer();
