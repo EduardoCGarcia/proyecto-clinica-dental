b0a5623a1bd81e388d85531e351d676e4d385841
EduardoCGarcia <echavezg004@gmail.com>
2023-10-19 15:03:27 -0600

    cambio de nombre de tabla en modelo

--- backend/models/mysql/users.js
+++ backend/models/mysql/users.js
@@ -2,7 +2,7 @@ const {sequelize} = require("../../config/mysql");
 const { DataTypes } = require("sequelize");
 
 const Usuario = sequelize.define(
-    "usuario",
+    "usuarios",
     {
         nombre:{
             type:DataTypes.STRING,
